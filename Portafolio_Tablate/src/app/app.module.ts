d82fdbf97f6fe4cadd83a4a83bb23e1e8626769f
ALFREDO <109168653+AlfredoTablate@users.noreply.github.com>
2023-02-08 13:33:52 -0300

    social
    siempre una lucha_social

--- Portafolio_Tablate/src/app/app.module.ts
+++ Portafolio_Tablate/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialesComponent } from './componnents/sociales/sociales.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialesComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
